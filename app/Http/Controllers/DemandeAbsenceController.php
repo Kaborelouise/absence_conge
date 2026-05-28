1ce37f274bc27af71ef5858c73775e967614fd85
Jessica <kaborejessicalouise@gmail.com>
2026-05-28 18:17:02 +0000

    Initialisation du projet

---
+++ app/Http/Controllers/DemandeAbsenceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DemandeAbsence;
+use Illuminate\Http\Request;
+
+class DemandeAbsenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DemandeAbsence $demandeAbsence)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DemandeAbsence $demandeAbsence)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DemandeAbsence $demandeAbsence)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DemandeAbsence $demandeAbsence)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/JustificatifAbsenceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JustificatifAbsence;
+use Illuminate\Http\Request;
+
+class JustificatifAbsenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JustificatifAbsence $justificatifAbsence)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JustificatifAbsence $justificatifAbsence)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JustificatifAbsence $justificatifAbsence)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JustificatifAbsence $justificatifAbsence)
+    {
+        //
+    }
+}
